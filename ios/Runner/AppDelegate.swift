ca3d251ac368b9466d45bb729b60df29167d5856
Kaze Shindo <kaze.bknb@outlook.jp>
2022-04-12 22:13:21 +0900

    feature: call swift-camera-function from flutter

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,29 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller :FlutterViewController = window?.rootViewController as! FlutterViewController
+    let cameraChannel = FlutterMethodChannel(name: "test_camera",
+                                            binaryMessenger: controller.binaryMessenger)
+
+    cameraChannel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        switch call.method {
+        case "getCamera":
+          self.receiveCamera(result: result,controller: controller)
+        default:
+          result(FlutterMethodNotImplemented)
+          return
+        }
+    })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+ func receiveCamera(result: FlutterResult, controller: FlutterViewController) {
+    let pickerController = UIImagePickerController()
+    pickerController.sourceType = .camera
+
+    controller.present(pickerController,animated: true,completion: nil)
   }
 }

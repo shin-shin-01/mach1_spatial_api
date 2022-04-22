0bdda00319aeab0e85a2337bf8894fe7fedb92a3
Kaze Shindo <kaze.bknb@outlook.jp>
2022-04-22 16:40:11 +0900

    add: comment

--- ios/Runner/CMDeviceMotion.swift
+++ ios/Runner/CMDeviceMotion.swift
@@ -6,11 +6,14 @@ import CoreMotion
 
 extension CMDeviceMotion {
     
+    // UIInterfaceOrientation: https://developer.apple.com/documentation/uikit/uiinterfaceorientation
+    // デバイスの向き: 横に倒しているか, 直立させているか, 及びホームボタンがどちら側にあるか
     func gaze(atOrientation orientation: UIInterfaceOrientation) -> SCNVector4 {
         
         let attitude = self.attitude.quaternion
         let aq = GLKQuaternionMake(Float(attitude.x), Float(attitude.y), Float(attitude.z), Float(attitude.w))
         
+        // SCNVector4: https://developer.apple.com/documentation/scenekit/scnvector4
         let final: SCNVector4
         
         switch orientation {
